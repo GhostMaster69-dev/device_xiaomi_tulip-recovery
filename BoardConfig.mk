0194820d77373723a11fa758fb4cf2ae80629216
me-cafebabe <me_cafebabe@hotmail.com>
2022-08-05 05:55:16 +0800

    tulip: Enable reboot to EDL option
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -104,6 +104,7 @@ TW_DEFAULT_BRIGHTNESS := 1433
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_EXTRA_LANGUAGES := true
+TW_HAS_EDL_MODE := true
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_NTFS_3G := true
