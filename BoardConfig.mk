be8ed80978ec6a4265854ce62755a4bc74dcb227
Captain Throwback <captainthrowback@hotmail.com>
2023-08-25 11:05:43 -0400

    tulip: remove unneeded items
    - libxml2 is built by default
    - fscrypt policy is set automatically
    
    Change-Id: I253cc780ff2a5ab74d74b1ddf74c9fa7fbe52039
    Signed-off-by: GhostMaster69-dev <rathore6375@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,12 +95,11 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 TARGET_OTA_ASSERT_DEVICE := tulip
 
 # Libraries
-TARGET_RECOVERY_DEVICE_MODULES += android.hidl.base@1.0 libdisplayconfig.qti libicuuc libion.so libxml2.so vendor.display.config@1.0 vendor.display.config@2.0
+TARGET_RECOVERY_DEVICE_MODULES += android.hidl.base@1.0 libdisplayconfig.qti libicuuc libion.so vendor.display.config@1.0 vendor.display.config@2.0
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/android.hidl.base@1.0.so \
    $(TARGET_OUT_SHARED_LIBRARIES)/libicuuc.so \
    $(TARGET_OUT_SHARED_LIBRARIES)/libion.so \
-   $(TARGET_OUT_SHARED_LIBRARIES)/libxml2.so \
    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/libdisplayconfig.qti.so \
    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@1.0.so \
    $(TARGET_OUT_SYSTEM_EXT_SHARED_LIBRARIES)/vendor.display.config@2.0.so
@@ -124,7 +123,6 @@ TW_SUPPORT_INPUT_AIDL_HAPTICS := true
 TW_THEME := portrait_hdpi
 TW_Y_OFFSET := 89
 TW_H_OFFSET := -89
-TW_USE_FSCRYPT_POLICY := 1
 TW_USE_TOOLBOX := true
 TW_OVERRIDE_SYSTEM_PROPS := "ro.build.date.utc;ro.bootimage.build.date.utc=ro.build.date.utc;ro.odm.build.date.utc=ro.build.date.utc;ro.product.build.date.utc=ro.build.date.utc;ro.system.build.date.utc=ro.build.date.utc;ro.system_ext.build.date.utc=ro.build.date.utc;ro.vendor.build.date.utc=ro.build.date.utc;ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
 
